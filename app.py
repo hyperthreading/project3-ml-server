504ab68fef2e33b42902357777833db69be3220f
hyperthreading <no1expman@gmail.com>
2018-01-10 21:54:26 +0900

    Resolved CORS issue on API

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request
+from flask_cors import cross_origin
 app = Flask(__name__)
 
 
@@ -9,6 +10,7 @@ def hello_world():
 
 
 @app.route('/result/<request_id>', methods=['POST'])
+@cross_origin('http://localhost:3000', supports_credentials=True)
 def receive_file(request_id):
     """
     Receive the image file user posted to use trained model.
@@ -18,5 +20,3 @@ def receive_file(request_id):
     file = request.files['file']
     file.save(os.path.join('./', file.filename))
     return 'got it'
-
-
